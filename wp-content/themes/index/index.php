550d9e36bf702f40fb4f9525675e99c7d06c5be9
Server <server@initialise.co>
2015-07-31 09:56:06 -0400

    commit of files on server

---
+++ wp-content/themes/index/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<title><?php wp_title( '|', true, 'right' ); ?></title>
+<link rel="stylesheet" type="text/css" href="<?php echo bloginfo('template_directory').'/style.css'; ?>">
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script type="text/javascript" src="<?php echo bloginfo('template_directory').'/js/randomColor.js'; ?>"></script>
+</head>
+<body <?php body_class(); ?>>
+<header class="header">
+	
+</header>
+<div id="container">
+    <div id="content">
+
+
+
+	</div>
+</div>
+<!-- Template for setting up columns and the like
+<div class="row">
+		<div class="large-12 medium-12 small-12 columns">
+		    <div class="header-top">
+		        <div class="branding"><a class="logo" href="<?php echo site_url(); ?>"></a></div>
+		        <ul class="social-media-top">
+		            <li><a href="" id="facebook-top"></a></li>
+		            <li><a href="" id="instagram-top"></a></li>
+		            <li><a href="" id="twitter-top"></a></li>
+		        </ul>
+		    </div>
+		</div>
+	</div>
+	-->
